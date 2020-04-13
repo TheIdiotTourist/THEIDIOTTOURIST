26a37c5571f7df109b683f72cd67e704143adcfd
Jake-thegangle-Snider <jakesniderlmt@gmail.com>
2020-04-13 14:39:48 -0400

    modularized

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,6 @@
 $(document).ready(function() {
     // This will fire when document is ready:
-    $(window).resize(function() {
-        // This will fire each time the window is resized:
-        if ($(window).width() <= 991) {
-            // if larger or equal
-            $(".hoverHide").hover(function() {
-                $(".hide").css("display", "none");
-            }, function() {
-                $(".hide").css("display", "block");
-            })
-        }
-        if ($(window).width() >= 992) {
-            $(".hoverHide").hover(function() {
-                $(".hide").css("display", "block");
-            })
-        }
-    });
-    $(window).resize();
-    $(".navbar-toggler").click(function() {
-        $(".navbar-toggler").toggleClass("navbar-toggler-position", "button:focus");
-    });
+    $('#tertNav').load("../html/partials/tertiary_nav.html"),
+        $('#tertHead').load("../html/partials/tertiary_head.html"),
+        $('#footerDiv').load("../html/partials/footer.html");
 });
