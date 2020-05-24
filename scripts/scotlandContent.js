21059aa5a995c8e17c06b1affc038eae4904afba
TheIdiotTourist <theidiottourist@gmail.com>
2020-05-24 14:13:35 -0400

    scotScript

--- scripts/scotlandContent.js
+++ scripts/scotlandContent.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
     //SCOTLAND INSERTS
-    $('#authNote').load("../../html/scotland/scotlandAuthorNote.html"),
-        $('#day1').load("../../html/scotland/scotlandDay1.html"),
-        $('#day2').load("../../html/scotland/scotlandDay2.html"),
-        $('#day3').load("../../html/scotland/scotlandDay3.html"),
-        $('#day4').load("../../html/scotland/scotlandDay4.html"),
-        $('#day5').load("../../html/scotland/scotlandDay5.html"),
-        $('#day6').load("../../html/scotland/scotlandDay6.html"),
-        $('#day7').load("../../html/scotland/scotlandDay7.html"),
-        $('#day8').load("../../html/scotland/scotlandDay8.html"),
-        $('#day9').load("../../html/scotland/scotlandDay9.html"),
-        $('#day10').load("../../html/scotland/scotlandDay10.html"),
-        $('#day11').load("../../html/scotland/scotlandDay11.html"),
-        $('#day12').load("../../html/scotland/scotlandDay12.html"),
-        $('#day13').load("../../html/scotland/scotlandDay13.html"),
-        $('#scotlandNav').load("../../html/scotland/scotlandNav.html");
+    $('#authNote').load("../html/scotland/scotlandAuthorNote.html"),
+        $('#day1').load("../html/scotland/scotlandDay1.html"),
+        $('#day2').load("../html/scotland/scotlandDay2.html"),
+        $('#day3').load("../html/scotland/scotlandDay3.html"),
+        $('#day4').load("../html/scotland/scotlandDay4.html"),
+        $('#day5').load("../html/scotland/scotlandDay5.html"),
+        $('#day6').load("../html/scotland/scotlandDay6.html"),
+        $('#day7').load("../html/scotland/scotlandDay7.html"),
+        $('#day8').load("../html/scotland/scotlandDay8.html"),
+        $('#day9').load("../html/scotland/scotlandDay9.html"),
+        $('#day10').load("../html/scotland/scotlandDay10.html"),
+        $('#day11').load("../html/scotland/scotlandDay11.html"),
+        $('#day12').load("../html/scotland/scotlandDay12.html"),
+        $('#day13').load("../html/scotland/scotlandDay13.html"),
+        $('#scotlandNav').load("../html/scotland/scotlandNav.html");
 });
 
 // URL updates and the element focus is maintained
