9cc2e2fd4a36a2e817c9dd3a98d429cd7da820e4
TheIdiotTourist <theidiottourist@gmail.com>
2020-05-24 12:00:21 -0400

    working index scripts?

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     // This will fire when document is ready:
-    $('#indexHead').load("./html/partials/index_head.html"),
-        $('#indexNav').load("./html/partials/index_nav.html"),
-        $('#idiotCarousel').load("./html/partials/carousel.html"),
-        $('#tertNav').load("../html/partials/tertiary_nav.html"),
-        $('#tertHead').load("../html/partials/tertiary_head.html"),
-        $('#footerDiv').load("./html/partials/footer.html");
+    $('#indexHead').load("./html/PARTIALS/index_head.html"),
+        $('#indexNav').load("./html/PARTIALS/index_nav.html"),
+        $('#idiotCarousel').load("./html/PARTIALS/carousel.html"),
+        $('#tertNav').load("../html/PARTIALS/tertiary_nav.html"),
+        $('#tertHead').load("../html/PARTIALS/tertiary_head.html"),
+        $('#footerDiv').load("./html/PARTIALS/footer.html");
 });
